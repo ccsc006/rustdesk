dc3c809df7715c6fcb9e08ee845363b810de16f9
ccsc006 <50624363+ccsc006@users.noreply.github.com>
2024-10-27 21:14:34 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["1.94.224.41"];
+pub const PUBLIC_RS_PUB_KEY: &str = "V3e3cBgAOxBqABlZ8VZ+lH9vBMkwAuBSdiWhsx1t7qQ=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
